9d8f1cae4baad501958179f8e9ab17ba69bb9015
DelpinSusaiRaj <delpinsusairajm@rajasri.net>
2018-05-22 19:35:23 +0530

    Computer Vision added

---
+++ XamarinComputerVision/XamarinComputerVision.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("XamarinComputerVision.Droid.MainApplication, XamarinComputerVision.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md520109d557d85487bca603bca66793642.MainApplication.class, md520109d557d85487bca603bca66793642.MainApplication.__md_methods);
+		
+	}
+}
